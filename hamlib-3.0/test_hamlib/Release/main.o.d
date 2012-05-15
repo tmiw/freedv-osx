fcd09227f87bff47b6020870dd90fde436c7aff3
wittend99 <wittend99@01035d8c-6547-0410-b346-abe4f91aad63>
2012-05-15 19:11:26 +0000

    synching things up...
    git-svn-id: https://svn.code.sf.net/p/freetel/code/fdmdv2@469 01035d8c-6547-0410-b346-abe4f91aad63

---
+++ hamlib-3.0/test_hamlib/Release/main.o.d
@@ -0,0 +1,102 @@
+Release/main.o: \
+ C:/Users/wittend/Projects/Radio/fdmdv2/hamlib-3.0/test_hamlib/main.cpp \
+ C:\bin\wxWidgets-2.9.2\include/wx/init.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/defs.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/platform.h \
+ C:\bin\wxWidgets-2.9.2\lib\gcc_dll\mswu/wx/setup.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/msw/libraries.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/msw/gccpriv.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/chkconf.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/msw/chkconf.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/version.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/cpp.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/dlimpexp.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/debug.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/chartype.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/windowid.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/msw/winundef.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/features.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/string.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/wxcrtbase.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/strvararg.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/strconv.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/buffer.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/fontenc.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/unichar.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/stringimpl.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/beforestd.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/afterstd.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/stringops.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/iosfwrap.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/crt.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/wxcrt.h \
+ C:\bin\wxWidgets-2.9.2\include/wx/wxcrtvararg.h ../include/hamlib/rig.h \
+ ../include/hamlib/riglist.h ../include/hamlib/rig_dll.h
+
+C:\bin\wxWidgets-2.9.2\include/wx/init.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/defs.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/platform.h:
+
+C:\bin\wxWidgets-2.9.2\lib\gcc_dll\mswu/wx/setup.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/msw/libraries.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/msw/gccpriv.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/chkconf.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/msw/chkconf.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/version.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/cpp.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/dlimpexp.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/debug.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/chartype.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/windowid.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/msw/winundef.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/features.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/string.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/wxcrtbase.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/strvararg.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/strconv.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/buffer.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/fontenc.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/unichar.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/stringimpl.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/beforestd.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/afterstd.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/stringops.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/iosfwrap.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/crt.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/wxcrt.h:
+
+C:\bin\wxWidgets-2.9.2\include/wx/wxcrtvararg.h:
+
+../include/hamlib/rig.h:
+
+../include/hamlib/riglist.h:
+
+../include/hamlib/rig_dll.h:
