40a9a7a6caaf31bcdd701c859b8d6105c1d5e846
wittend99 <wittend99@01035d8c-6547-0410-b346-abe4f91aad63>
2012-06-18 15:07:01 +0000

    git-svn-id: https://svn.code.sf.net/p/freetel/code/fdmdv2@570 01035d8c-6547-0410-b346-abe4f91aad63

--- libsndfile/Projects/w32-mingw/Release/main.o.d
+++
@@ -1,5 +0,0 @@
-Release/main.o: \
- C:/Users/wittend/Projects/Radio/fdmdv2/libsndfile/Projects/w32-mingw/main.cpp \
- C:/Users/wittend/Projects/Radio/fdmdv2/libsndfile/Projects/w32-mingw/sndfile.h
-
-C:/Users/wittend/Projects/Radio/fdmdv2/libsndfile/Projects/w32-mingw/sndfile.h:
