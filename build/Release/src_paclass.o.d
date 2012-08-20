8e7814ab6d499cb97c3f40bf52359986ab700db6
wittend99 <wittend99@01035d8c-6547-0410-b346-abe4f91aad63>
2012-08-20 19:54:14 +0000

    git-svn-id: https://svn.code.sf.net/p/freetel/code/fdmdv2@631 01035d8c-6547-0410-b346-abe4f91aad63

--- build/Release/src_paclass.o.d
+++
@@ -1,7 +0,0 @@
-Release/src_paclass.o: C:/bin/Projects/Radio/fdmdv2/src/paclass.cpp \
- C:/bin/Projects/Radio/fdmdv2/src/paclass.h \
- ../3rdparty/portaudio/include/portaudio.h
-
-C:/bin/Projects/Radio/fdmdv2/src/paclass.h:
-
-../3rdparty/portaudio/include/portaudio.h:
