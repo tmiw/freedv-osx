9fe08e40cd1d61a9e1c4f21ae3e54ba854a246a9
wittend99 <wittend99@01035d8c-6547-0410-b346-abe4f91aad63>
2012-12-17 02:43:05 +0000

    Unversion some pieces unintentionally added by restructuring.
    git-svn-id: https://svn.code.sf.net/p/freetel/code/fdmdv2@1145 01035d8c-6547-0410-b346-abe4f91aad63

--- build/codelite/Release/src_varicode.o.d
+++
@@ -1,7 +0,0 @@
-Release/src_varicode.o: D:/Projects/Radio/fdmdv2/src/varicode.c \
- D:/Projects/Radio/fdmdv2/src/varicode.h \
- D:/Projects/Radio/fdmdv2/src/varicode_table.h
-
-D:/Projects/Radio/fdmdv2/src/varicode.h:
-
-D:/Projects/Radio/fdmdv2/src/varicode_table.h:
